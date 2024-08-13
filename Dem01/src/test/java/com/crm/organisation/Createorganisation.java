259f28456365aa3446b74ad804f83267e82ffe97
Kuilo562 <atulsharma99856@gmail.com>
2024-08-13 14:14:14 +0530

    Update Createorganisation.java

--- Dem01/src/test/java/com/crm/organisation/Createorganisation.java
+++ Dem01/src/test/java/com/crm/organisation/Createorganisation.java
@@ -3,5 +3,6 @@ package com.crm.organisation;
 public class Createorganisation {
 public static void main(String[] args) {
 	System.out.println("organisation created");
+	System.out.println("organisation created");
 }
 }
